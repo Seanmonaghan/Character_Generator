0f3b4625b4819161f23539dbbdc704ee77da4a27
Sean Monaghan <smonagha@conncoll.edu>
2021-04-26 20:27:36 -0400

    fixed spelling error

--- contollers/api/gameRoutes.js
+++ contollers/api/gameRoutes.js
@@ -17,7 +17,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
-        const gameData = await Game.destory({
+        const gameData = await Game.destroy({
             where: {
                 id: req.params.id,
                 user_id: req.sessions.user_id
