e5aa33866f22186140d0d361ea84ab753795affa
Sean Monaghan <smonagha@conncoll.edu>
2021-05-01 10:06:27 -0400

    edit failed, reverting

--- models/index.js
+++ models/index.js
@@ -3,27 +3,39 @@ const Character = require('./Character');
 const Game = require('./Game');
 
 User.hasMany(Game, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 });
 
 Game.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 })
 
 User.hasMany(Character, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 });
 
 Character.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 });
 
 Game.hasMany(Character, {
-    foreignKey: 'game_id'
+    foreignKey: 'game_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 });
 
 Character.belongsTo(Game, {
-    foreignKey: 'game_id'
+    foreignKey: 'game_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 });
 
 module.exports = { User, Character, Game }
