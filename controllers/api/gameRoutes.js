76d5bf0262937e85d2343c4c2897ea1e7b4c7c50
Sean Monaghan <smonagha@conncoll.edu>
2021-05-04 21:37:36 -0400

    delete button fixed

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -20,7 +20,7 @@ router.delete('/:id', withAuth, async (req, res) => {
         const gameData = await Game.destroy({
             where: {
                 id: req.params.id,
-                user_id: req.sessions.user_id
+                user_id: req.session.user_id
             }
         });
 
